34a339e4084e865c9ea61a5c6667d3e9155c0e25
Datafox-JCP <datafox@outlook.com>
2024-09-19 14:21:27 -0600

    Proyecto Inicial

---
+++ Mensual/Mensual/MensualApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensualApp.swift
+//  Mensual
+//
+//  Created by Juan Hernandez Pazos on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MensualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mensual/MensualWidget/MensualWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MensualWidgetBundle.swift
+//  MensualWidget
+//
+//  Created by Juan Hernandez Pazos on 19/09/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MensualWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MensualWidget()
+    }
+}
